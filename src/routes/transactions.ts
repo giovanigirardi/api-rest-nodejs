dde1f7f4b154d073fe07fe31dbbc3a2593d9e129
Giovani Girardi <giovanigg08@gmail.com>
2024-09-01 20:20:25 -0300

    feat(Cookies):

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -48,10 +48,21 @@ export async function transactionsRoutes(server: FastifyInstance) {
 
 		const { amount, title, type } = createTransactionBodySchema.parse(request.body);
 
+		let sessionId = request.cookies.sessionId;
+
+		if (!sessionId) {
+			sessionId = randomUUID();
+			reply.setCookie("sessionId", sessionId, {
+				path: "/",
+				maxAge: 60 * 60 * 24 * 7, // 7 days
+			});
+		}
+
 		await knex("transactions").insert({
 			id: randomUUID(),
 			title,
 			amount: type === "income" ? amount : amount * -1,
+			session_id: sessionId,
 		});
 
 		reply.status(201).send();
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
+import cookie from "@fastify/cookie";
 import fastify from "fastify";
 import { env } from "./env";
 import { transactionsRoutes } from "./routes/transactions";
 
 const server = fastify();
 
+server.register(cookie);
 server.register(transactionsRoutes, {
 	prefix: "/transactions",
 });
