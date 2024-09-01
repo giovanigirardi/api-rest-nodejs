b3ca1807c8a5609b48b9093a12d329b943cbe470
Giovani Girardi <giovanigg08@gmail.com>
2024-09-01 20:04:01 -0300

    feat(Transactions): :sparkles: Created list and read transactions endpoints

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -6,6 +6,29 @@ import { knex } from "../database";
 
 // biome-ignore lint/suspicious/useAwait: Fastify plugins need to be async functions
 export async function transactionsRoutes(server: FastifyInstance) {
+	server.get("/", async () => {
+		const transactions = await knex("transactions").select();
+
+		return { transactions };
+	});
+
+	server.get("/:id", async (request, reply) => {
+		const getTransactionParamsSchema = z.object({
+			id: z.string().uuid(),
+		});
+
+		const { id } = getTransactionParamsSchema.parse(request.params);
+
+		const transaction = await knex("transactions").select().where({ id }).first();
+
+		if (!transaction) {
+			reply.status(404).send({ message: "Transaction not found" });
+			return;
+		}
+
+		return { transaction };
+	});
+
 	server.post("/", async (request, reply) => {
 		const createTransactionBodySchema = z.object({
 			title: z.string(),
