693d0e99a14e23bc90b59297639f23857dccd466
Giovani Girardi <giovanigg08@gmail.com>
2024-09-01 19:01:32 -0300

    feat: :sparkles: created .env file

--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,16 @@
+import "dotenv/config";
 import { knex as setupKnex } from "knex";
 
 import type { Knex } from "knex";
 
+if (!process.env.DATABASE_URL) {
+	throw new Error("DATABASE_URL is not set");
+}
+
 export const config: Knex.Config = {
 	client: "sqlite3",
 	connection: {
-		filename: "./db/app.db",
+		filename: process.env.DATABASE_URL,
 	},
 	useNullAsDefault: true,
 	migrations: {
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,9 @@ import { knex } from "./database";
 const server = fastify();
 
 server.get("/hello", async () => {
-	const tables = await knex("sqlite_schema").select("*");
+	const transactions = await knex("transactions").where("id", "d3734136-b1ef-468a-a75e-908e0cca4de9").select("*");
 
-	return tables;
+	return transactions;
 });
 
 server.listen({ port: 3333 });
